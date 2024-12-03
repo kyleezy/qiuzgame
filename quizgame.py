bafa7e44ac36431d68a8258152158d8f378eca56
kyleezy <kyleezywakabi@gmail.com>
2024-12-03 20:18:17 +0100

    Add files via upload

--- quizgame.py
+++ quizgame.py
@@ -5,13 +5,15 @@ TITLE="quiz"
 msgbox=Rect(0,0,800,100)
 qesbox=Rect(0,100,600,150)
 timbox=Rect(620,100,160,150)
+box1=Rect(0,260,280,180)
+box2=Rect(320,260,280,180)
 def draw():
     screen.fill("black")
     screen.draw.filled_rect(msgbox,"black")
     screen.draw.filled_rect(qesbox,"blue")
     screen.draw.filled_rect(timbox,"red")
-
-
+    screen.draw.filled_rect(box1,"yellow")
+    screen.draw.filled_rect(box2,"yellow")
     
 
 
